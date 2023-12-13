fb811c4272322b8c4dbebeb9c35c1fd03d4ee7ea
Ejelonu Benedict <ejelonu@kobo360.com>
2023-12-13 11:59:06 +0100

    created sample hello_world.py script

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+import datetime
+
+def main():
+
+    current_time = datetime.datetime.now()
+    print(f"Hello from Bwhiz, the time is currently : {current_time}")
+
+if __name__ == '__main__':
+    main()
